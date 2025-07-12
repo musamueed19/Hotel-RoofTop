2775ba1433a9de36fd81f4b32d8845c3f66f89d7
Muhammad Musa Mueed <musavuswh@gmail.com>
2025-07-12 19:30:13 +0500

    imporve blogCards styling

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const BlogCard = ({ item }) => {
-    const {
+  const {
     title,
     description,
     author,
@@ -14,17 +14,15 @@ const BlogCard = ({ item }) => {
     publishedDate,
     tags,
     readingTime,
-    } = item;
-    
-    const [imgSrc, setImgSrc] = useState(coverImg);
+  } = item;
+
+  const [imgSrc, setImgSrc] = useState(coverImg);
 
   return (
     <Link to={`/${_id}`}>
       <div className="h-full flex flex-col gap-y-4 bg-white p-4 rounded-xl">
-       
-       
-              {/* img */}
-        <div className="h-fit w-full flex items-center justify-center">
+        {/* img */}
+        <div className="w-full flex items-start justify-center">
           <img
             src={imgSrc}
             alt={`${title} img`}
@@ -32,38 +30,41 @@ const BlogCard = ({ item }) => {
             className="object-contain rounded-xl overflow-hidden shadow-sm w-full"
           />
         </div>
-        
-              
-              {/* categories */}
+
+        {/* categories */}
         {/* text-link/88 bg-gray-200/75 */}
+
         <div className="bg-blue-200/60 text-[15px] py-1 px-2 rounded-xl text-blue-600 font-bold w-fit capitalize border-blue-200 border">
           {category}
-              </div>
-              
+        </div>
 
         {/* title */}
-              <h2 className="text-[#292929] font-bold text-lg">{title}</h2>
-              
-
+        <h2 className="text-[#292929] font-bold text-lg">{title}</h2>
 
         {/* content */}
         <p className="text-link">{content}</p>
         {/* author, publishedDate, comments or bookmark */}
-              <div className="flex items-center justify-between">
-                  {/* author */}
-                  <div className="flex items-center gap-x-2">
-                      <div className="size-10">
-                          <img src={authorImg} alt="authorImg" className="object-cover rounded-full overflow-hidden" />
-                      </div>
+        <div className="h-full flex items-end justify-between">
+          {/* author */}
+          <div className="flex items-center gap-x-2">
+            <div className="size-10">
+              <img
+                src={authorImg}
+                alt="authorImg"
+                className="object-cover rounded-full overflow-hidden"
+              />
+            </div>
 
-                      {/* author name, published Date */}
-                      <div className="h-fit">
-                          <p className="text-[#292929] font-bold">{author}</p>
-                          <p className="text-sm text-link/70 font-medium">{publishedDate}</p>
-                      </div>
-                  </div>
+            {/* author name, published Date */}
+            <div className="h-fit">
+              <p className="text-[#292929] font-bold">{author}</p>
+              <p className="text-sm text-link/70 font-medium">
+                {publishedDate}
+              </p>
+            </div>
+          </div>
 
-                  {/* bookmarks, comments */}
+          {/* bookmarks, comments */}
         </div>
       </div>
     </Link>
